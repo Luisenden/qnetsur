a98f9c7696a9b164ebb4cd31e48a7147e893e0b0
Luise Prielinger <l.p.prielinger@tudelft.nl>
2024-02-07 12:57:05 +0100

    adapted delta

--- usecase_qswitch/vsgridsearch.py
+++ usecase_qswitch/vsgridsearch.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     delta = 0
     # do grid search until maximum time is reached
     sim = Simulation(simwrapper, simulation_qswitch, vals=vals, vars=vars)
-    while time_tracker + time_delta < max_time:
+    while time_tracker + delta < max_time:
         start = time.time()
         x = sim.get_random_x(1)
         eval = sim.run_sim(x)
@@ -42,7 +42,7 @@ if __name__ == '__main__':
         evals.append(evalset)
         times_tracked.append(time.time()-start)
         time_tracker = np.sum(times_tracked)
-        time_delta = np.mean(times_tracked)
+        delta = np.mean(times_tracked)
     
     gridsearch = pd.DataFrame.from_records(evals)
     with open(f'../../surdata/qswitch/GS_qswitch_nleafnodes{NLEAF_NODES}_{MAX_TIME:.2f}h_objective-servernode_SEED{SEED_OPT}_'+datetime.now().strftime("%m-%d-%Y_%H:%M:%S")+'.pkl', 'wb') as file:
