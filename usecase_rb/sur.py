d793510b5e12071d2149aee12f6862d990a7b546
Luise Prielinger <l.p.prielinger@tudelft.nl>
2024-04-11 10:51:43 +0200

    updated rb usecase

--- usecase_rb/sur.py
+++ usecase_rb/sur.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
         sur = Surrogate(simwrapper, simulation_rb, sample_size=sample_size, vals=vals, vars=vars, k=6)
         sur.optimize(max_time=max_time, verbose=False)
         
-        with open(f'../../surdata/rb/SU_starlight_{MAX_TIME:.1f}h_objective-budget_SEED{SEED}_'+datetime.now().strftime("%m-%d-%Y_%H:%M:%S")+'.pkl', 'wb') as file:
+        with open(f'../../surdata/rb_budget/SU_starlight_{MAX_TIME:.1f}h_objective-budget_SEED{SEED}_'+datetime.now().strftime("%m-%d-%Y_%H:%M:%S")+'.pkl', 'wb') as file:
                 pickle.dump(sur, file)
